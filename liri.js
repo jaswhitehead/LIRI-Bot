a0798f0034600bc3f713417f65d075af24fc925a
Jason Whitehead <jaswhitehead@gmail.com>
2020-06-15 13:38:38 -0500

    Created if/then statements to run the two process.argv's

--- liri.js
+++ liri.js
@@ -10,6 +10,21 @@ var keys = require("./keys.js");
 var spotifyKeys = new spotify(keys.spotify);
 
 var command = process.argv[2]
-var commandArg = process.argv[3];
+var commandTwo = process.argv[3];
 
 function concertThis(artist)
+
+
+
+
+if (command === "concert-this") {
+    concertThis(commadTwo);
+} else if (command === "spotify-this-song") {
+    spotifyThisSong(commandTwo);
+} else if (command === "movie-this") {
+    movieThis(commandTwo);
+} else if (command === "do-what-it-says") {
+    done();
+} else {
+    console.log("invalid command");
+}
